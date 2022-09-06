6bafe60bdba6e3272cd8f7d568023f407fa00639
shachem <sekki.hachem@outlook.com>
2022-09-06 12:27:21 +0100

    Implement nav-bar and footer to the homepage

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h2>List of Contacts</h2>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Contacts</a>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Greetings</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, eaque
@@ -12,6 +15,7 @@ export default function Home() {
         dolore quibusdam? Suscipit aut sint aliquid quisquam dicta corrupti
         voluptatibus quo doloribus.
       </p>
+      <Footer />
     </div>
   );
 }
