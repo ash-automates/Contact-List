5bf3aa32c0cbfeaf73d5dc5f213e8166aca0e611
shachem <sekki.hachem@outlook.com>
2022-09-06 10:32:08 +0100

    setup 3 different routes for the webpage

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, maxime
+        explicabo. Hic doloribus earum error eos laborum beatae ea rerum
+        molestias sunt veritatis eum ipsam cum, quam corrupti qui repudiandae.
+      </p>
+    </div>
+  );
+};
+
+export default About;
+<div className="container">
+  <h1>About me</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, maxime
+    explicabo. Hic doloribus earum error eos laborum beatae ea rerum molestias
+    sunt veritatis eum ipsam cum, quam corrupti qui repudiandae.
+  </p>
+</div>;
---
+++ pages/contacts/index.js
@@ -0,0 +1,22 @@
+const Contacts = () => {
+  return (
+    <div className="container">
+      <h1>Contacts</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quod
+        illo aliquam. Voluptas veritatis explicabo ex rerum placeat, maxime
+        magni facere voluptatibus numquam eligendi sequi enim rem! Ea, ipsam
+        optio. Blanditiis veniam, qui laborum excepturi est natus, libero
+        delectus provident explicabo mollitia saepe beatae aspernatur cumque
+        obcaecati ab dignissimos reiciendis laudantium laboriosam voluptatem
+        nisi sit molestiae expedita! Sunt, cumque quibusdam? Ipsa, facere
+        cupiditate. Similique deserunt minima, quis sit facilis temporibus
+        commodi! Officiis aliquam voluptate enim. Fugit eos eaque quasi quia,
+        dignissimos eum aliquam, dolores voluptatibus nobis, possimus earum eius
+        blanditiis!
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
