40e67732833d8fb15ea54a436aa5f9333283465b
shachem <sekki.hachem@outlook.com>
2022-09-07 18:15:10 +0100

    fetch contact data from API & display it

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,20 +1,26 @@
-const Contacts = () => {
+export const getStaticProps = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      contacts: data,
+    },
+  };
+};
+
+const Contacts = ({ contacts }) => {
   return (
     <div className="container">
-      <h1>Contacts</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quod
-        illo aliquam. Voluptas veritatis explicabo ex rerum placeat, maxime
-        magni facere voluptatibus numquam eligendi sequi enim rem! Ea, ipsam
-        optio. Blanditiis veniam, qui laborum excepturi est natus, libero
-        delectus provident explicabo mollitia saepe beatae aspernatur cumque
-        obcaecati ab dignissimos reiciendis laudantium laboriosam voluptatem
-        nisi sit molestiae expedita! Sunt, cumque quibusdam? Ipsa, facere
-        cupiditate. Similique deserunt minima, quis sit facilis temporibus
-        commodi! Officiis aliquam voluptate enim. Fugit eos eaque quasi quia,
-        dignissimos eum aliquam, dolores voluptatibus nobis, possimus earum eius
-        blanditiis!
-      </p>
+      <h1>All Contacts</h1>
+      {contacts.map((contact) => {
+        return (
+          <div key={contact.id}>
+            <a>
+              <h3>{contact.name}</h3>
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };
