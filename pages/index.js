1dd7886cca75010e60ee48b5a457debdd367aa3d
shachem <sekki.hachem@outlook.com>
2022-09-07 11:41:58 +0100

    Add styling to the navbar, footer and homepage

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Greetings</h1>
-      <p>
+      <h1 className={styles.title}>Greetings</h1>
+      <p className={styles.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, eaque
         aspernatur sapiente corrupti suscipit officiis aliquid sed repudiandae
         dolore quibusdam? Suscipit aut sint aliquid quisquam dicta corrupti
         voluptatibus quo doloribus.
       </p>
       <Link href="/contacts/">
-        <a>See contacts</a>
+        <a className={styles.btn}>See contacts</a>
       </Link>
     </div>
   );
