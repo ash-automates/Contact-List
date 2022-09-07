0d17cf560e5679153798dd831d9022a830b97820
shachem <sekki.hachem@outlook.com>
2022-09-07 12:39:28 +0100

    Redirect the user home from 404 page in 3 sec

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 const NotFound = () => {
+  const history = useRouter();
+  useEffect(
+    () => {
+      setTimeout(() => {
+        history.push("/");
+      }, 3000);
+    },
+    /* eslint-disable */
+    []
+    /* eslint-enable */
+  );
   return (
     <div className="not-found">
       <h1>Oops</h1>
       <h2>Page cannot be found!</h2>
       <p>
-        Go back{" "}
+        You&apos;ll be redirected to the{" "}
         <Link href="/">
-          <a>Home</a>
-        </Link>
+          <a>homepage</a>
+        </Link>{" "}
+        in 3 seconds...
       </p>
     </div>
   );
