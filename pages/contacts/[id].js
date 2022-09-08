85714198c5394c8e36846f3d902957dd9d184b69
shachem <sekki.hachem@outlook.com>
2022-09-08 11:32:14 +0100

    Create data-specific static page for each contact

--- pages/contacts/[id].js
+++ pages/contacts/[id].js
@@ -1,7 +1,35 @@
-const Details = () => {
+export const getStaticPaths = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  const paths = users.map((user) => {
+    return {
+      params: {
+        id: user.id.toString(),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async function (context) {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+  return {
+    props: { user },
+  };
+};
+
+const Details = ({ user }) => {
   return (
-    <div className="details">
-      <h2>Details</h2>
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.website}</p>
+      <p>{user.address.suite + ", " + user.address.street}</p>
     </div>
   );
 };
